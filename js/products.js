a938a631f56afb604eb04fdb797c98b983b6f587
matonp93 <matiasnp12@gmail.com>
2023-08-11 19:27:25 -0300

    Trabajamos en estilos de productos.html y products.js para crear divs

--- js/products.js
+++ js/products.js
@@ -15,17 +15,33 @@ function listadoProductos(){
             let h3 = document.createElement("h3");
             h3.innerHTML+= element.name + " - " + element.currency + " " + element.cost;
             let p1 = document.createElement("p");
+            p1.classList.add('descripcion')
             p1.innerHTML+=element.description;
             let p2 = document.createElement("p");
+            p2.classList.add('vendidos')
             p2.innerHTML+=element.soldCount+ " vendidos";
-            let nDiv = document.createElement("div");
+            
+            /* Contenedores y clase de divs*/
+            let containerDiv = document.createElement("div")
+            containerDiv.classList.add('productcard')
+            let imgDiv = document.createElement("div");
+            imgDiv.classList.add('imgdiv')
+            let h3Div = document.createElement("div");
+            h3Div.classList.add('h3div')
+            let pDiv = document.createElement("div");
+            pDiv.classList.add('pdiv')
+            let p2Div = document.createElement("div")
+            p2Div.classList.add('p2div')
             let image = document.createElement("img");
             image.setAttribute("src",element.image);
-            nDiv.appendChild(image);
-            nDiv.appendChild(h3);
-            nDiv.appendChild(p1);
-            nDiv.appendChild(p2);
-            products.appendChild(nDiv);
+            imgDiv.appendChild(image);
+            h3Div.appendChild(h3);
+            h3Div.appendChild(p1);
+            p2Div.appendChild(p2);
+            containerDiv.appendChild(imgDiv)
+            containerDiv.appendChild(h3Div)
+            containerDiv.appendChild(p2Div)
+            products.appendChild(containerDiv);
         });
 
     })
