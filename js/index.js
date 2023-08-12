79b37a1fbbcefbdee3deb35d46346b0d86716d3c
JulianCastill0 <juli.castillo909@gmail.com>
2023-08-11 21:44:57 -0300

    Agregue logica al login

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
+const btnDeslogear = document.getElementById("deslogear");
+
 document.addEventListener("DOMContentLoaded", function(){
+    function comprobarLogin(){
+        return ((localStorage.getItem("email") != null) && (localStorage.getItem("password") != null))
+    }
+
+    if (!comprobarLogin()){
+        location.href = "login.html"
+    } 
+
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
@@ -11,4 +21,9 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+
+    btnDeslogear.addEventListener("click", () => {
+        localStorage.clear();
+        location.href = "login.html"
+    })
 });
